a84923166754e75d9002fd1e7b907bd759ef8fbc
xiangxu <iasonliu@gmail.com>
2022-04-08 09:46:09 +0800

    init server

---
+++ server/src/main.rs
@@ -0,0 +1,53 @@
+use axum::{response::IntoResponse, routing::get, Router};
+use clap::Parser;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use std::str::FromStr;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+
+// Setup the command line interface with clap.
+#[derive(Parser, Debug)]
+#[clap(name = "server", about = "A server for our wasm project!")]
+struct Opt {
+    // set the log level
+    #[clap(short = 'l', long = "log", default_value = "debug")]
+    log_level: String,
+    /// set the listen addr
+    #[clap(short = 'a', long = "addr", default_value = "::1")]
+    addr: String,
+
+    /// set the listen port
+    #[clap(short = 'p', long = "port", default_value = "8080")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Opt::parse();
+    // Setup logging & RUST_LOG from args
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level))
+    }
+    // enable console logging
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(hello))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+
+    let sock_addr = SocketAddr::from((
+        IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
+        opt.port,
+    ));
+
+    log::info!("listening on http://{}", sock_addr);
+
+    axum::Server::bind(&sock_addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Unable to start server");
+}
+
+async fn hello() -> impl IntoResponse {
+    "hello from server!"
+}
